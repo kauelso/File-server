923ce56d4fba27c2b6b36830ebb406bd173d8bd2
Kaue Lucas <kauelucas@terra.com.br>
2021-05-16 16:59:58 -0300

    recept string problem

--- server/server.c
+++ server/server.c
@@ -1,60 +1,87 @@
-// Server side C/C++ program to demonstrate Socket programming
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 1024
+
+void write_file(int socket, char* buffer){
+	int n;
+	FILE *fp;
+	char *filename = "text.txt";
+
+	fp = fopen(filename,"w");
+	while (1)
+	{
+		n = recv(socket,buffer,BUFFER_SIZE,0);
+		if(n <= 0){
+			break;
+		}
+		printf("%s\n",buffer);
+		fprintf(fp,"%s",buffer);
+		bzero(buffer,BUFFER_SIZE);
+	}
+	fclose(fp);
+	return;
+}
 
 int main(int argc, char const *argv[])
 {
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[1024] = {0};
-	char *hello = "Hello from server";
+	char * ip = "127.0.0.1";
+	int socketfd, new_socket;
+	struct sockaddr_in sv_address, new_addr;
+	socklen_t addr_size;
+	int addrlen = sizeof(sv_address);
+	char buffer[1024];
+	bzero(buffer,BUFFER_SIZE);
 	
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	// Criar o file descriptor do socket
+	//AF_INET é uma familia de endereços que permite o socket se comunicar com
+	//o protocolo ipv4
+	//SOCK_STREAM protocolo de conecção TCP
+	if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
+		perror("Erro ao criar socket");
+		exit(1);
 	}
+
+	printf("Socket criado...\n");
 	
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,&opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
+	sv_address.sin_family = AF_INET;
+	sv_address.sin_addr.s_addr = inet_addr(ip);
+	sv_address.sin_port = PORT;
 	
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address,
-								sizeof(address))<0)
+	//Coloca o endereço no socket
+	if (bind(socketfd, (struct sockaddr *)&sv_address,sizeof(sv_address))<0)
 	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
+		perror("bind falhou");
+		exit(1);
 	}
-	if (listen(server_fd, 3) < 0)
+
+	printf("Bind feito com sucesso...\n");
+
+	if (listen(socketfd, 10) < 0)
 	{
-		perror("listen");
-		exit(EXIT_FAILURE);
+		perror("Nao foi possivel ouvir outras conexoes");
+		exit(1);
+	}else{
+		printf("Listening...");
 	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-					(socklen_t*)&addrlen))<0)
+
+	addr_size = sizeof(new_addr);
+
+	if ((new_socket = accept(socketfd, (struct sockaddr *)&new_addr,&addr_size))<0)
 	{
-		perror("accept");
-		exit(EXIT_FAILURE);
+		perror("accept error");
+		exit(1);
 	}
-	valread = read( new_socket , buffer, 1024);
-	printf("%s\n",buffer );
-	send(new_socket , hello , strlen(hello) , 0 );
-	printf("Hello message sent\n");
+
+	write_file(new_socket, buffer);
+	printf("Arquivo escrito com sucesso!\n");
+
 	return 0;
 }
